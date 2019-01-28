5f041a92c1beffb23303a7746471ed70521ec43c
Milan Lund <hello@milanlund.com>
2019-01-28 21:06:40 +0100

    Refactoring of creating Url map

--- helpers/urlMap.js
+++ helpers/urlMap.js
@@ -1,12 +1,74 @@
 const { DeliveryClient } = require('kentico-cloud-delivery');
 const { deliveryConfig } = require('../config');
 
+const getMapItem = (data, fields) => {
+    let item = {};
+
+    fields.forEach(field => {
+        switch (field) {
+            case 'codename':
+                item.codename = data.codename;
+                break;
+            case 'url':
+                item.url = data.url;
+                break;
+            case 'date': 
+                item.date = data.date;
+                break;
+        }; 
+    });
+
+    return item;
+};
+
+const typeLevels = {
+    home: {
+        urlLength: 0
+    },
+    navigation_item: {
+        urlLength: 1
+    },
+    scenario: {
+        urlLength: 2
+    },
+    topic: {
+        urlLength: 3
+    },
+    article: {
+        urlLength: 4
+    }
+};
+
+const createUrlMap = (response, fields, url, urlMap = []) => {
+    let node = '';
+
+    if (response.items) node = 'items';
+    if (response.navigation) node = 'navigation';
+    if (response.children) node = 'children';
+
+    response[node].forEach(item => {
+        if (item.elements.url) {
+            url.length = typeLevels[item.system.type].urlLength;
+            url[url.length - 1] = item.elements.url.value;
+        }
+
+        urlMap.push(getMapItem({
+            codename: item.system.codename,
+            url: `/${url.join('/')}`,
+            date: item.system.last_modified
+        }, fields));
+
+        createUrlMap(item, fields, url, urlMap);         
+    });
+
+    return urlMap;
+};
+
 const getUrlMap = async (config) => {
     deliveryConfig.projectId = config.projectid;
-    const previewApiKey = config.previewapikey;
 
-    if (previewApiKey) {
-        deliveryConfig.previewApiKey = previewApiKey;
+    if (config.previewapikey) {
+        deliveryConfig.previewApiKey = config.previewapikey;
         deliveryConfig.enablePreviewMode = true;
     }
 
@@ -19,75 +81,13 @@ const getUrlMap = async (config) => {
     const response = await query
         .getPromise();
 
-    let urlMap = [];
-
-    const getMapItem = (data, fields) => {
-        let item = {};
-
-        fields.forEach(field => {
-            switch (field) {
-                case 'codename':
-                    item.codename = data.codename;
-                    break;
-                case 'url':
-                    item.url = data.url;
-                    break;
-                case 'date': 
-                    item.date = data.date;
-                    break;
-            }; 
-        });
-
-        return item;
-    };
-
     let fields = ['codename', 'url'];
 
     if (config.isSitemap) {
         fields = ['url', 'date'];
     }
 
-    response.items.forEach(home => {
-        urlMap.push(getMapItem({
-            codename: home.system.codename,
-            url: `/`,
-            date: home.system.last_modified
-        }, fields));
-
-        home.navigation.forEach(navigationItem => {
-            urlMap.push(getMapItem({
-                codename: navigationItem.system.codename,
-                url: `/${navigationItem.elements.url.value}`,
-                date: navigationItem.system.last_modified
-            }, fields));
-
-            navigationItem.children.forEach(scenario => {
-                urlMap.push(getMapItem({
-                    codename: scenario.system.codename,
-                    url: `/${navigationItem.elements.url.value}/${scenario.elements.url.value}`,
-                    date: scenario.system.last_modified
-                }, fields));
-
-                scenario.children.forEach(topic => {
-                    urlMap.push(getMapItem({
-                        codename: topic.system.codename,
-                        url: `/${navigationItem.elements.url.value}/${scenario.elements.url.value}/${topic.elements.url.value}`,
-                        date: topic.system.last_modified
-                    }, fields));
-
-                    topic.children.forEach(article => {
-                        urlMap.push(getMapItem({
-                            codename: article.system.codename,
-                            url: `/${navigationItem.elements.url.value}/${scenario.elements.url.value}/${topic.elements.url.value}/${article.elements.url.value}`,
-                            date: article.system.last_modified
-                        }, fields));
-                    });
-                });
-            });
-        });
-    });
-
-    return urlMap;
+    return createUrlMap(response, fields, []);
 };
 
 module.exports = getUrlMap;
