1e0ab5a2d515c8ca2d90c5c1fff4932bd99a9bed
Milan Lund <hello@milanlund.com>
2019-03-06 15:57:03 +0100

    Feedback form

---
+++ public/js/src/custom/feedback.js
@@ -0,0 +1,32 @@
+(() => {
+    let yesBtn = document.querySelector('.feedback__button--yes');
+    let noBtn = document.querySelector('.feedback__button--no');
+    let yesMsg = document.querySelector('.feedback__message--yes');
+    let noMsg = document.querySelector('.feedback__message--no');
+    let btnArea = document.querySelector('.feedback__answer');
+
+    yesMsg.classList.add('feedback__message--hidden');
+    noMsg.classList.add('feedback__message--hidden');
+
+    const sendFeedback = (value) => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'click',
+            'eventCategory': 'Helpful',
+            'eventAction': window.location.pathname,
+            'value': value
+        });
+    };
+
+    const onBtnClick = (btn, msg, value) => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            btnArea.parentElement.removeChild(btnArea);
+            msg.classList.remove('feedback__message--hidden');
+            sendFeedback(value);
+        });
+    };
+
+    onBtnClick(yesBtn, yesMsg, 'yes');
+    onBtnClick(noBtn, noMsg, 'no');
+})();
