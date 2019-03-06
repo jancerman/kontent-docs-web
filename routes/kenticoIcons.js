973d34a0d63bd8665bdf9a5f4c18af373fe472e4
Milan Lund <hello@milanlund.com>
2019-03-06 15:25:47 +0100

    ES lint fix

--- routes/kenticoIcons.js
+++ routes/kenticoIcons.js
@@ -5,11 +5,11 @@ const asyncHandler = require('express-async-handler');
 
 router.get('/', asyncHandler(async (req, res, next) => {
     let icons = await axios.get('https://cdn.jsdelivr.net/gh/Kentico/kentico-icons/production/icon-variables.less');
-    
+
     let lines = icons.data.split('\n');
     let css = '';
 
-    for(let i = 0;i < lines.length;i++){
+    for (let i = 0; i < lines.length; i++) {
         let rule = lines[i].split(':')
         if (rule.length === 2) {
             css += `${rule[0] ? rule[0].replace('@', '.') : ''}:before{content:${rule[1] ? rule[1].trim().replace(';', '') : ''}}`;
