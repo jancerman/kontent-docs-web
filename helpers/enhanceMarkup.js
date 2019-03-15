7ce31b1cc9ca82102181dfe72a10a6100add089d
Matus Backor <MatusB@kentico.com>
2019-03-15 10:37:24 +0100

    Refactor code to fix codebeat issues

--- helpers/enhanceMarkup.js
+++ helpers/enhanceMarkup.js
@@ -3,26 +3,36 @@ const cheerio = require('cheerio');
 const enhanceMarkup = (text) => {
     const $ = cheerio.load(text);
 
-    $('p.kc-linked-item-wrapper, p:empty').each(function(i, elem) {
+    replaceNodeWithItsContent($, 'p.kc-linked-item-wrapper, p:empty');
+    setWidthToImages($);
+    processLinks($);
+
+    let output = $.html();
+    return output.replace('<html><head></head><body>', '').replace('</body></html>', '');
+};
+
+const replaceNodeWithItsContent = ($, selector) => {
+    $(selector).each(function(i, elem) {
         var contents = $(this).contents();
         $(this).replaceWith(contents);
     });
+};
 
+const setWidthToImages = ($) => {
     $('img[data-asset-id]').each(function(i, elem) {
         var $that = $(this);
         if (!$that.attr('src').endsWith('.gif')) {
             $that.attr('src', $that.attr('src') + '?w=926');
         }
     });
+};
 
+const processLinks = ($) => {
     $(`a[data-item-id][href=""]`).each(function(i, elem) {
         var $that = $(this);
         $that.removeAttr('data-item-id');
         $that.attr('href', '/page-not-found');
     });
-
-    let output = $.html();
-    return output.replace('<html><head></head><body>', '').replace('</body></html>', '');
 };
 
 module.exports = enhanceMarkup;
--- helpers/linksResolverTemplates.js
+++ helpers/linksResolverTemplates.js
@@ -14,32 +14,42 @@ const linksResolverTemplates = {
     },
     resolveInnerRichTextLinks: (item, urlMap) => {
         Object.keys(item)
-            .filter((key) => item.hasOwnProperty(key) && item[key].hasOwnProperty('type') && item[key].hasOwnProperty('links') && item[key].type === `rich_text`)
-            .forEach((key) => {
-                item[key].links.forEach((link) => {
-                    let resolvedUrl = urlMap.filter(elem => elem.codename === link.codename);
-                    if (resolvedUrl.length > 0) {
-                        resolvedUrl = resolvedUrl[0].url;
-                    } else if (link.type === 'article') {
-                        resolvedUrl = `/other/${item.urlSlug}`;
-                    } else {
-                        resolvedUrl = '/page-not-found';
-                    }
-                    if (item[key].value && resolvedUrl) {
-                        const $ = cheerio.load(item[key].value);
-                        $(`a[data-item-id="${link.itemId}"]`).each(function(i, elem) {
-                            var $that = $(this);
-                            $that.removeAttr('data-item-id');
-                            $that.attr('href', resolvedUrl);
-                        });
-                        item[key].value = $.html();
-                        item[key].value = item[key].value.replace('<html><head></head><body>', '').replace('</body></html>', '');
-                    }
-                });
-            });
+            .filter((key) =>
+                item.hasOwnProperty(key) &&
+                item[key].hasOwnProperty('type') &&
+                item[key].hasOwnProperty('links') &&
+                item[key].type === `rich_text`)
+            .forEach((key) => resolveLinkUrlsInElement(item[key], item, urlMap));
 
         return item;
     }
+};
+
+const resolveLinkUrlsInElement = (element, item, urlMap) => {
+    element.links.forEach((link) => {
+        let resolvedUrl = urlMap.filter(elem => elem.codename === link.codename);
+        if (resolvedUrl.length > 0) {
+            resolvedUrl = resolvedUrl[0].url;
+        } else if (link.type === 'article') {
+            resolvedUrl = `/other/${item.urlSlug}`;
+        } else {
+            resolvedUrl = '/page-not-found';
+        }
+        if (element.value && resolvedUrl) {
+            updateLinkAttribute(element, resolvedUrl, link)
+        }
+    });
+};
+
+const updateLinkAttribute = (element, resolvedUrl, link) => {
+    const $ = cheerio.load(element.value);
+    $(`a[data-item-id="${link.itemId}"]`).each(function (i, elem) {
+        var $that = $(this);
+        $that.removeAttr('data-item-id');
+        $that.attr('href', resolvedUrl);
+    });
+    element.value = $.html();
+    element.value = element.value.replace('<html><head></head><body>', '').replace('</body></html>', '');
 };
 
 module.exports = linksResolverTemplates;
