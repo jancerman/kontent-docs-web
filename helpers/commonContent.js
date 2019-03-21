7030ad6968d39b3801cabb9d0e48ca5e9d1b4d11
Milan Lund <hello@milanlund.com>
2019-03-21 07:45:55 +0100

    Enforce platforms order in selector

--- helpers/commonContent.js
+++ helpers/commonContent.js
@@ -22,6 +22,25 @@ const commonContent = {
             projectid: res.locals.projectid,
             previewapikey: res.locals.previewapikey
         });
+    },
+    orderPlatforms: (platforms) => {
+        if (platforms) {
+            let order = ['rest', '_net', 'javascript', 'typescript', 'java', 'android', 'ios', 'php', 'ruby'];
+            let result = [];
+
+            order.forEach(orderItem => {
+                platforms.forEach(platformItem => {
+                    let codename = platformItem.codename || platformItem.platform.value[0].codename;
+                    if (orderItem === codename) {
+                        result.push(platformItem);
+                    }
+                });
+            });
+
+            return result;
+        }
+
+        return platforms;
     }
 }
 
