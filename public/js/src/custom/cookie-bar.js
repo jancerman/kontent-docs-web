857c868968940743c1416f9cc339cc719ecdbeef
Milan Lund <hello@milanlund.com>
2019-05-21 11:08:14 +0200

    Text for cookies bar

--- public/js/src/custom/cookie-bar.js
+++ public/js/src/custom/cookie-bar.js
@@ -1,8 +1,12 @@
 (function () {
     if (localStorage) {
         if (localStorage.getItem('KCDOCS.cookieBar') !== 'true') {
-            var bar = '<div class="cookie-bar js-cookie-bar"><div class="cookie-bar__container"><div class="cookie-bar__inner"><div class="cookie-bar__text">The website uses small cookies to improve your website experience. You may disable them from your browser settings at any time. <a href="https://kenticocloud.com/cookies-policy" target="_blank" rel="noopener noreferrer">Learn more</a>.</div><div class="cookie-bar__close js-cookie-bar__close">×</div></div></div></div>';
+            var bar = '<div class="cookie-bar js-cookie-bar"><div class="cookie-bar__container"><div class="cookie-bar__inner"><div class="cookie-bar__text"></div><div class="cookie-bar__close js-cookie-bar__close">×</div></div></div></div>';
             document.querySelector('.footer').insertAdjacentHTML('afterend', bar);
+
+            if (UIMessages && UIMessages.cookieBar) {
+                document.querySelector('.cookie-bar__text').innerHTML = helper.decodeHTMLEntities(UIMessages.cookieBar);
+            }
         }
 
         var closeEl = document.querySelector('.js-cookie-bar__close');
