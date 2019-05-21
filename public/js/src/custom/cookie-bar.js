1dcced32a8a3e77f8faf651822856d9709df09cf
Milan Lund <hello@milanlund.com>
2019-05-21 10:56:55 +0200

    Cookies bar design

--- public/js/src/custom/cookie-bar.js
+++ public/js/src/custom/cookie-bar.js
@@ -1,7 +1,7 @@
 (function () {
     if (localStorage) {
         if (localStorage.getItem('KCDOCS.cookieBar') !== 'true') {
-            var bar = '<div class="cookie-bar js-cookie-bar"><div class="cookie-bar__inner"><div class="cookie-bar__text">The website uses small cookies to improve your website experience. You may disable them from your browser settings at any time. <a href="https://kenticocloud.com/cookies-policy" target="_blank" rel="noopener noreferrer">Learn more</a>.</div><div class="cookie-bar__close js-cookie-bar__close">×</div></div></div>';
+            var bar = '<div class="cookie-bar js-cookie-bar"><div class="cookie-bar__container"><div class="cookie-bar__inner"><div class="cookie-bar__text">The website uses small cookies to improve your website experience. You may disable them from your browser settings at any time. <a href="https://kenticocloud.com/cookies-policy" target="_blank" rel="noopener noreferrer">Learn more</a>.</div><div class="cookie-bar__close js-cookie-bar__close">×</div></div></div></div>';
             document.querySelector('.footer').insertAdjacentHTML('afterend', bar);
         }
 
