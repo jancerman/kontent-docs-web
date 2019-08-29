c1a083c35edd1ba7dd3c209a1752a8c6b3a3426a
Milan Lund <hello@milanlund.com>
2019-08-29 08:47:25 +0200

    Added preview warning banner

---
+++ public/js/src/custom/preview-warning.js
@@ -0,0 +1,20 @@
+(() => {
+    const warning = document.querySelector('.preview-warning');
+
+    if (warning) {
+        let links = warning.querySelectorAll('a');
+
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].innerHTML === 'live version') {
+                const loc = window.location;
+                let href = links[i].getAttribute('href');
+
+                if (href.endsWith('/')) {
+                    href = href.slice(0, -1);
+                }
+
+                links[i].setAttribute('href', href + loc.pathname + loc.search + loc.hash);
+            }
+        }
+    }
+})();
