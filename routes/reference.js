15aeca147c180500d242c71715747c06c77f7eea
Milan Lund <hello@milanlund.com>
2019-11-11 09:54:00 +0100

    Fix reference subNavigation cache key naming

--- routes/reference.js
+++ routes/reference.js
@@ -18,7 +18,7 @@ const getUrlMap = require('../helpers/urlMap');
 const handleArticle = async (settings, req, res) => {
     settings.renderSettings.view = 'apiReference/pages/reference';
     const parentSlug = req.originalUrl.split('/')[1];
-    const subNavigation = await handleCache.evaluateSingle(res, `subNavigation_${req.params.slug}`, async () => {
+    const subNavigation = await handleCache.evaluateSingle(res, `subNavigation_${parentSlug}`, async () => {
         return await commonContent.getSubNavigation(res, parentSlug);
     });
     const platformsConfig = await platforms.getPlatformsConfig(res);
