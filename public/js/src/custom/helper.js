0806fa97936c01285c918bfca1f3bde7e946f2e9
Milan Lund <hello@milanlund.com>
2020-01-10 15:46:55 +0100

    Display mode implementation

--- public/js/src/custom/helper.js
+++ public/js/src/custom/helper.js
@@ -294,6 +294,45 @@ window.helper = (() => {
         return recaptchaKey;
     };
 
+    const nextUntil = (elem, selector, filter, skip) => {
+        // matches() polyfill
+        if (!Element.prototype.matches) {
+            Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+        }
+
+        // Setup siblings array
+        var siblings = [];
+
+        // Get the next sibling element
+        elem = elem.nextElementSibling;
+
+        // As long as a sibling exists
+        while (elem) {
+            // If we've reached our match, bail
+            if (elem.matches(selector)) break;
+
+            // If filtering by a selector, check if the sibling matches
+            if (filter && !elem.matches(filter)) {
+                elem = elem.nextElementSibling;
+                continue;
+            }
+
+            // If filtering by a selector, check if the sibling matches
+            if (skip && elem.matches(skip)) {
+                elem = elem.nextElementSibling;
+                continue;
+            }
+
+            // Otherwise, push it to the siblings array
+            siblings.push(elem);
+
+            // Get the next sibling element
+            elem = elem.nextElementSibling;
+        }
+
+        return siblings;
+    };
+
     return {
         getParents: getParents,
         findAncestor: findAncestor,
@@ -313,7 +352,8 @@ window.helper = (() => {
         setCookie: setCookie,
         getCookie: getCookie,
         eraseCookie: eraseCookie,
-        loadRecaptcha: loadRecaptcha
+        loadRecaptcha: loadRecaptcha,
+        nextUntil: nextUntil
     }
 })();
 
