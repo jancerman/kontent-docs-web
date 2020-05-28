b116c9b512d12583d99d83c9164109cb193c2eef
Milan Lund <hello@milanlund.com>
2020-05-28 16:58:13 +0200

    Api2Pdf - Improved algorithm for headings page breaks

--- public/js/src/custom/pdf.js
+++ public/js/src/custom/pdf.js
@@ -1,15 +1,32 @@
-
 (() => {
     if (!window.helper.getParameterByName('pdf')) return;
     const headings = document.querySelectorAll('.article__content h2, .article__content h3, .article__content h4');
 
     for (let i = 0; i < headings.length; i++) {
-        const nextElem = headings[i].nextSibling;
+        if (headings[i].parentNode.classList.contains('page-break-prevent')) continue;
+
+        const nextElems = [];
+        let sibling = headings[i];
+
+        do {
+            if (sibling) {
+                sibling = sibling.nextSibling;
+            }
+            nextElems.push(sibling);
+        } while (sibling && sibling.nodeType === 1 && sibling.tagName.startsWith('H') && sibling.tagName.length === 2)
+
+        console.log(nextElems)
+
         const div = document.createElement('div');
         div.style.pageBreakInside = 'avoid';
+        div.classList.add('page-break-prevent');
         headings[i].insertAdjacentElement('afterend', div);
-
         div.appendChild(headings[i]);
-        div.appendChild(nextElem);
+
+        for (let j = 0; j < nextElems.length; j++) {
+            if (nextElems[j]) {
+                div.appendChild(nextElems[j]);
+            }
+        }
     }
 })();
