1c53f342b0e01ffb08ac94856d257105ea2656d4
Milan Lund <hello@milanlund.com>
2020-11-27 13:07:23 +0100

    E-learning - Preview course ID support

--- helpers/trainingCourse.js
+++ helpers/trainingCourse.js
@@ -3,6 +3,7 @@ const commonContent = require('./commonContent');
 const handleCache = require('./handleCache');
 const helper = require('./helperFunctions');
 const lms = require('./lms')
+const isPreview = require('./isPreview');
 
 const isCourseAvailable = (user) => {
   if (user.email.endsWith('@kentico.com')) {
@@ -22,91 +23,104 @@ const isCourseAvailable = (user) => {
   }
 
   return false;
-}
+};
+
+const getTrainingCourseInfoFromLMS = async (user, courseId, UIMessages) => {
+    if (!courseId && courseId !== 0) return null;
+    // Register user in LMS and course and get info about course url and completion
+    const courseInfo = await lms.handleTrainingCourse(user, courseId);
+    let text = '';
+    let renderAs = 'button';
+
+    if (courseInfo.completion === 0) {
+      text = UIMessages.training___cta_start_course.value;
+    } else if (courseInfo.completion === 100) {
+      text = UIMessages.training___cta_revisit_course.value;
+    } else if (courseInfo.completion === 101) {
+      text = 'User info is not available in LMS.';
+      renderAs = 'text';
+    } else if (courseInfo.completion === 102) {
+      text = 'Course info is not available in LMS.';
+      renderAs = 'text';
+    } else if (courseInfo.completion === 103) {
+      text = 'Course ID does not exist in LMS.';
+      renderAs = 'text';
+    } else {
+      text = UIMessages.training___cta_resume_course.value;
+    }
+
+    return {
+      text: text,
+      url: courseInfo.url,
+      completion: courseInfo.completion.toString(),
+      certificate: courseInfo.certificate,
+      target: courseInfo.target,
+      signedIn: true,
+      renderAs: renderAs
+    };
+};
 
 const getTrainingCourseInfo = async (content, req, res) => {
   const UIMessagesObj = await handleCache.ensureSingle(res, 'UIMessages', async () => {
     return await commonContent.getUIMessages(res);
   });
 
-  const UIMessages = UIMessagesObj && UIMessagesObj.length ? UIMessagesObj[0] : null
+  const UIMessages = UIMessagesObj && UIMessagesObj.length ? UIMessagesObj[0] : null;
+  let renderGeneralMessage = false;
+  let forcePreviewRender = false;
+  let generalMessage;
+  let user;
 
   // If user is not authenticated
   if (!req.oidc.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
-    return {
+    renderGeneralMessage = true;
+    generalMessage = {
       text: UIMessages.sign_in_button.value,
       url: '/login',
       renderAs: 'button'
     };
-  }
-
-  // Get additional info about authenticated user
-  const user = await axios({
-    method: 'get',
-    url: `${process.env['SubscriptionService.Url']}${req.oidc.user.email}/`,
-    headers: {
-      Authorization: `Bearer ${process.env['SubscriptionService.Bearer']}`
-    }
-  });
-
-  if (!user) {
-    return {
-      text: 'User is not available in the subscription service',
-      renderAs: 'text',
-      signedIn: true
-    };
-  }
-
-  if (helper.isCodenameInMultipleChoice(content.display_options.value, 'hide_cta')) {
-    return {
-      text: UIMessages.training___cta_coming_soon.value,
-      renderAs: 'text',
-      signedIn: true
-    };
-  }
-
-  // If user has access to courses
-  if (!isCourseAvailable(user.data)) {
-    return {
-      text: UIMessages.training___cta_buy_course.value,
-      url: UIMessages.training___cta_buy_link.value,
-      renderAs: 'button',
-      signedIn: true
-    };
-  }
-
-  // Register user in LMS and course and get info about course url and completion
-  const courseInfo = await lms.handleTrainingCourse(user.data, content.talentlms_course_id.value);
-  let text = '';
-  let renderAs = 'button';
-
-  if (courseInfo.completion === 0) {
-    text = UIMessages.training___cta_start_course.value;
-  } else if (courseInfo.completion === 100) {
-    text = UIMessages.training___cta_revisit_course.value;
-  } else if (courseInfo.completion === 101) {
-    text = 'User info is not available in LMS.';
-    renderAs = 'text';
-  } else if (courseInfo.completion === 102) {
-    text = 'Course info is not available in LMS.';
-    renderAs = 'text';
-  } else if (courseInfo.completion === 103) {
-    text = 'Course ID does not exist in LMS.';
-    renderAs = 'text';
   } else {
-    text = UIMessages.training___cta_resume_course.value;
+      // Get additional info about authenticated user
+      user = await axios({
+        method: 'get',
+        url: `${process.env['SubscriptionService.Url']}${req.oidc.user.email}/`,
+        headers: {
+          Authorization: `Bearer ${process.env['SubscriptionService.Bearer']}`
+        }
+      });
+
+      if (!user) {
+        renderGeneralMessage = true;
+        generalMessage = {
+          text: 'User is not available in the subscription service',
+          renderAs: 'text',
+          signedIn: true
+        };
+      } else if (helper.isCodenameInMultipleChoice(content.display_options.value, 'hide_cta')) {
+        renderGeneralMessage = true;
+        forcePreviewRender = true;
+        generalMessage = {
+          text: UIMessages.training___cta_coming_soon.value,
+          renderAs: 'text',
+          signedIn: true
+        };
+      } else if (!isCourseAvailable(user.data)) {
+        renderGeneralMessage = true;
+        generalMessage = {
+          text: UIMessages.training___cta_buy_course.value,
+          url: UIMessages.training___cta_buy_link.value,
+          renderAs: 'button',
+          signedIn: true
+        };
+      }
   }
 
   return {
-    text: text,
-    url: courseInfo.url,
-    completion: courseInfo.completion.toString(),
-    certificate: courseInfo.certificate,
-    target: courseInfo.target,
-    signedIn: true,
-    renderAs: renderAs
-  };
+    general: renderGeneralMessage ? generalMessage : null,
+    production: !renderGeneralMessage ? await getTrainingCourseInfoFromLMS(user.data, content.talentlms_course_id.value, UIMessages) : null,
+    preview: (!renderGeneralMessage || forcePreviewRender) && isPreview(res.locals.previewapikey) ? await getTrainingCourseInfoFromLMS(user.data, content.talentlms_course_id_preview.value, UIMessages) : null
+  }
 };
 
 module.exports = getTrainingCourseInfo;
